e29a978e6428a54c283c7507b33f455645883c6f
syonosuke <syonosuke1996@outlook.jp>
2023-10-18 11:40:59 +0900

    if文の演習

---
+++ case.rb
@@ -0,0 +1,26 @@
+#誕生石から、誕生月を出力するプログラム
+
+#stone = 'peridot'
+#if stone == 'ruby'
+#  puts '7月'
+#elsif stone == 'peridot'
+ # puts '8月'
+#elsif stone =='sapphire'
+#  puts '9月'
+#else 
+#  puts 'データが登録されていません'
+#end
+
+#case
+
+stone = 'ruby'
+case stone
+when 'ruby'
+  puts'7月'
+when 'peridot'
+  puts '8月'
+when 'sapphire'
+  puts '9月'
+else 
+  puts 'データが登録されてません'
+end
