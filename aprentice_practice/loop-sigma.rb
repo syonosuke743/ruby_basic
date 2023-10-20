cdbd8767680cbcad5c00306e83b3d1724326a2f2
syonosuke <syonosuke1996@outlook.jp>
2023-10-20 22:20:39 +0900

    総和まで

--- aprentice_practice/loop-sigma.rb
+++ aprentice_practice/loop-sigma.rb
@@ -1,7 +1,10 @@
 def sum(x,y)
   x.to_i
   y.to_i
-#xからyまでの足し算の合計を計算簡易シグマプログラム
+  total = 0 #初期値に0を設定
+  for i in x..y #xからｙまでの整数の範囲でループ
+    total += i #現在の整数iをtotalに加算。ｘが10なら11になって総和が更新
+  end
+  puts " #{total}"
 end
-
-sum (10,80)
+  sum(10,80)
