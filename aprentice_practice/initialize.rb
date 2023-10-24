53600495739bd77ff24481288139252bea443844
syonosuke <syonosuke1996@outlook.jp>
2023-10-24 23:02:26 +0900

    単一責任の原則

--- aprentice_practice/initialize.rb
+++ aprentice_practice/initialize.rb
@@ -1,29 +1,31 @@
 class VendingMachine
 
-def initialize(name, deposit)
-  @name = name
+def initialize(name, deposit) #初期化
+  @name = name 
   @deposit = deposit
 end
 
 
-def deposit_coin(deposit)
-  if deposit >= 100
-    deposit += 100
+def deposit_coin(deposit) #呼び出し元の引数を代入
+  if deposit >= 100 #depositが100以上なら
+    deposit += 100 #depositに100を加算
   end
 end
 
 def press_button
-  if @deposit >=100
-  puts "cider"
+  if @deposit >=100 #デポジットが100以上なら
+  puts "cider" #サイダーと出力
+  @deposit -= 100 #サイダーがでたらデポジットを-100する
   end
 end
-
 
 def press_manufacturer_name
   puts "#{@name}"
 end
 
 end
+
+
 
 vending_machine = VendingMachine.new('サントリー',100)
 puts vending_machine.press_button
